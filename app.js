548dadb77e574916231a8b4a6711c0b4c544b76f
Bo <ahsaira@tutamail.com>
2024-06-12 17:46:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const contractAddress = "0xcb868742037649d8caf18427307bc073e529ebe9 ";
+const contractAddress = "0xcb868742037649d8caf18427307bc073e529ebe9";
 const contractABI = [
     {
         "inputs": [
