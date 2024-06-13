d94fc4f9036fac734c6bf961fe317ba33f710f78
Bo <ahsaira@tutamail.com>
2024-06-13 15:53:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 
 // Define the contract address and ABI
-const contractAddress = '0xdf72ab181c97fa0979f1f22c3894f6d1356de0b8';
+const contractAddress = 0xdf72ab181c97fa0979f1f22c3894f6d1356de0b8;
 const contractABI = /* Insert your contract ABI here */;
 
 // Web3 provider
